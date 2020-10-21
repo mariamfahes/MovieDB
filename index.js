98ccb1f7a60f766ebab45232efe9a75d333043cf
mariamfahes <fahesmariam20@gmail.com>
2020-10-21 21:47:47 +0300

    step3 and step4

--- index.js
+++ index.js
@@ -9,3 +9,36 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get("/test", function (req, res) {
+    res.send({status:200, message:"ok"})
+    res.status(200).send('ok');
+})
+app.get('/time',function(req,res){
+   var d= new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var x = h >= 12 ? 'pm' : 'am';
+    h = h % 12;
+    h = h ? h : 12;
+    m = m < 10 ? '0'+m: m;
+    var time= h + ':' + m + ' ' + x;
+
+    res.send('{status:200, message:'+time+'}');
+    res.status(200).send(time);
+});
+ app.get("/hello",function(req,res){
+    res.status(200).send("Hello");
+});
+app.get("/hello/:id",function(req,res){
+    res.status(200).send("Hello, "+req.params.id);
+});
+app.get('/search',function(req,res){
+    if(req.query.s==""){
+        res.status(500).send("error:true, you have to provide a search");
+    }
+    else{
+        res.status(200).send("ok, data:"+req.query.s);
+
+    }
+}); 
