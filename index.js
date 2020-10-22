ac765ed9eb554c54306bd881990ec80b8d47ccc0
mariamfahes <fahesmariam20@gmail.com>
2020-10-22 11:49:55 +0300

    step5

--- index.js
+++ index.js
@@ -42,3 +42,34 @@ app.get('/search',function(req,res){
 
     }
 }); 
+
+app.get('/movies/read',function(req,res){
+    res.send({status:200, data:movies})
+
+});
+
+app.get('/movies/create',function(req,res){
+    res.send("cr")
+
+});
+
+app.get('/movies/update',function(req,res){
+res.send("up")
+});
+app.get('/movies/delete',function(req,res){
+res.send("del")
+});
+
+app.get('/', (req, res) => {
+  res.send('ok')
+});
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    {title: 'Brazil', year: 1985, rating: 8 },
+    {title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+    
+];
+
+
+
